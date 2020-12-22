d9a296ec94ed9bb45804b08f14b753420600225d
Trisnayudha <48747021+Trisnayudha@users.noreply.github.com>
2020-12-22 08:55:24 +0700

    Form Login

---
+++ lib/login.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FormLogin extends StatefulWidget {
+  @override
+  _FormLoginState createState() => _FormLoginState();
+}
+
+class _FormLoginState extends State<FormLogin> {
+  bool _securText = true;
+  TextEditingController userController = new TextEditingController();
+  TextEditingController passController = new TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final globalKey = GlobalKey<ScaffoldState>();
+
+  String user = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    "asset/logo.jpg",
+                    width: 300.0,
+                    height: 300.0,
+                  ),
+                  Divider(),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 5, 20, 0),
+                    child: TextFormField(
+                      controller: userController,
+                      decoration: InputDecoration(
+                        hintText: "Username",
+                        suffixIcon: Icon(
+                          Icons.account_box,
+                          color: Colors.blueAccent,
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          //DENGAN BORDER BERWARNA PINK
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                        labelText: "Username: ",
+                        labelStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return "Email diperlukan";
+                        } else {
+                          return null;
+                        }
+                      },
+                      onChanged: (String value) {
+                        user = value;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 5, 20, 0),
+                    child: TextFormField(
+                      controller: passController,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        suffixIcon: IconButton(
+                          icon: Icon(_securText
+                              ? Icons.remove_red_eye
+                              : Icons.security),
+                          onPressed: () {
+                            setState(() {
+                              _securText = !_securText;
+                            });
+                          },
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                        labelText: "Password: ",
+                        labelStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      obscureText: _securText,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (userController.text == 'trisnayudha' &&
+                            passController.text == 'yudha ganteng') {
+                        } else {
+                          showAlertDialogCupertino('Input!!!');
+                        }
+                      },
+                      color: Colors.blue[100],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0),
+                      ),
+                      padding: const EdgeInsets.all(0.0),
+                      child: Ink(
+                        child: Container(
+                          constraints: const BoxConstraints(
+                            minWidth: 36.0,
+                            minHeight: 36.0,
+                          ),
+                          alignment: Alignment.center,
+                          child: const Text(
+                            'Login',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )));
+  }
+
+  showAlertDialogCupertino(text) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: Text('Kesalahan'),
+          content: Text('Username atau Password Salah'),
+          actions: [
+            CupertinoDialogAction(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.pop(context);
+                print('Clicked No!');
+              },
+            ),
+          ],
+        );
+      },
+      barrierColor: Colors.black.withOpacity(0.5),
+      barrierDismissible: false,
+    );
+  }
+}
---
+++ lib/splashScreen.dart
@@ -0,0 +1,121 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'login.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) {
+          return FormLogin();
+        }),
+      );
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Center(
+              child: Image.asset(
+                "asset/logo.jpg",
+                width: 200.0,
+                height: 150.0,
+              ),
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Data',
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                      letterSpacing: .4,
+                      fontSize: 28.0,
+                      color: Colors.deepOrangeAccent,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              Text(
+                ' Ku',
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                      letterSpacing: .4,
+                      fontSize: 28.0,
+                      color: Colors.lightBlueAccent,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ],
+          ),
+          //Seharusnya ada dibawah
+          // Stack(
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: Text(
+                    "Copy Right",
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.green,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: Text(
+                    " Trisnayudha Bachtiar 2020",
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // ),
+        ],
+      ),
+    );
+  }
+}
+// body: Padding(
+//         padding: const EdgeInsets.all(16),
+//         child: Stack(
+//           children: <Widget>[
+//             Align(
+//               alignment: Alignment.bottomCenter,
+//               child: Text(
+//                 "Bottom Center",
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
